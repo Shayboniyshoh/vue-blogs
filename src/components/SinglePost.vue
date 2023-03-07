f411ffa07d94d6fedaaa590d6945e589f22e9170
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2023-03-07 17:50:56 +0500

    Created Post List and Single Post

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="post">
+        <h1>{{ post.title }}</h1>
+        <p>{{ post.body }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['post']
+}
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="home">
     <h1>Welcome to Blogs</h1>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
+import PostList from "../components/PostList.vue";
+import { ref } from "vue";
 
 export default {
   name: 'HomeView',
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      { id: 0, title: "News Heading 1", body: "Lorem ipsum, dolor sit amet consectetur adipisicing." },
+      { id: 1, title: "News Heading 2", body: "Lorem ipsum, dolor sit amet consectetur adipisicing." },
+      { id: 2, title: "News Heading 3", body: "Lorem ipsum, dolor sit amet consectetur adipisicing." }
+    ])
+
+    return { posts }
+  }
+
 }
 </script>
